fc7268d3aba90f05dab8e2fccb8e8ae7657e7bf2
kmassi69 <131153882+kmassi69@users.noreply.github.com>
2026-03-16 09:24:11 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 import { useAuthStore } from '../state/authStore';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: "https://ai-resume-analyzer-backend-4bzx.onrender.com",
   headers: {
     'Content-Type': 'application/json',
   },
